e29dff6b72cb71251890fa80ed0c22fe7aee7315
elizabeth.bylatova@gmail.com <elizabeth.bylatova@gmail.com>
2019-07-05 16:01:22 +0300

    fixed bugs in tests

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 
-
 from tests_abstract import *
 
 
@@ -14,14 +13,14 @@ class TestLoginPage(TestAbstract):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.driver.get('http://185.183.122.133/')  # login page
+        cls.driver.get(f"{HOST}")  # login page
 
     def test_title_in_page(self):
-        self.driver.get('http://185.183.122.133/')
+        self.driver.get(f"{HOST}")
         self.assertIn("Софит-Здоровье", self.driver.title)
 
     def test_login_in_page(self):
-        self.driver.get('http://185.183.122.133/')
+        self.driver.get(f"{HOST}")
         login = self.driver.find_element_by_id('id_username')
         login.send_keys("patient_for_tests")
         login.submit()
@@ -32,7 +31,7 @@ class TestLoginPage(TestAbstract):
             self.assert_(False, 'Element "errorlist not in page"')
 
     def test_password_in_page(self):
-        self.driver.get('http://185.183.122.133/')
+        self.driver.get(f"{HOST}")
         pswd = self.driver.find_element_by_id('id_password')
         pswd.send_keys("UzEMaxrc7D")
         pswd.submit()
@@ -43,23 +42,29 @@ class TestLoginPage(TestAbstract):
             self.assert_(False, 'Element "errorlist" not in page')
 
     def test_input_incorrect_login_password_in_page(self):
-        self.driver.get('http://185.183.122.133/')
+        self.driver.get(f"{HOST}")
         login = self.driver.find_element_by_id('id_username')
         login.send_keys("patient_for_tut")
         pswd = self.driver.find_element_by_id('id_password')
         pswd.send_keys("QWERTY")
         pswd.submit()
-        WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
-            (By.XPATH, "//div[@class='alert alert-dismissible fade show alert-warning']")))
+        try:
+            WebDriverWait(self.driver, 5).until(EC.presence_of_element_located(
+                (By.XPATH, "//div[@class='alert alert-dismissible fade show alert-warning']")))
+        except TimeoutException:
+            self.assert_(False, 'Element "alert alert-dismissible fade show alert-warning" not in page')
 
     def test_entry_in_page(self):
-        self.driver.get('http://185.183.122.133/')
+        self.driver.get(f"{HOST}")
         login = self.driver.find_element_by_id('id_username')
         login.send_keys("patient_for_tests")
         pswd = self.driver.find_element_by_id('id_password')
         pswd.send_keys('UzEMaxrc7D')
         pswd.submit()
-        WebDriverWait(self.driver, 5).until(EC.url_contains('http://185.183.122.133/patient/')) #url_to_be-?
+        try:
+            WebDriverWait(self.driver, 5).until(EC.url_to_be(f"{HOST}/patient/"))
+        except TimeoutException:
+            self.assert_(False, 'Try again')
 
 
 class TestHomePage(TestAbstractWithAuthorizationPatient):
@@ -67,40 +72,34 @@ class TestHomePage(TestAbstractWithAuthorizationPatient):
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.driver.get('http://185.183.122.133/patient/')
+        cls.driver.get(f"{HOST}/patient/")
 
     def test_title_in_page(self):
-        self.driver.get('http://185.183.122.133/patient/')
+        self.driver.get(f"{HOST}/patient/")
         self.driver.find_element_by_link_text('История').click()
-        self.assertIn('История', self.driver.page_source)
+        self.assertIn('История событий вашего личного кабинета', self.driver.page_source)
 
     def test_doctors_appointment_in_page(self):
-        self.driver.get('http://185.183.122.133/patient/')
+        self.driver.get(f"{HOST}/patient/")
         self.driver.find_element_by_link_text('Записаться').click()
-        self.assertIn('Записаться', self.driver.page_source)
+        self.assertIn('Выберите специализацию доктора', self.driver.page_source)
 
     def test_counseling_in_page(self):
-        self.driver.get('http://185.183.122.133/patient/')
+        self.driver.get(f"{HOST}/patient/")
         self.driver.find_element_by_link_text('Консультации').click()
-        self.assertIn('Консультации', self.driver.page_source)
+        self.assertIn('Все мои консультации', self.driver.page_source)
+        self.assertIn('Запланированные визиты', self.driver.page_source)
+        self.assertIn('Прошедшие визиты', self.driver.page_source)
 
     def test_my_file_in_page(self):
-        self.driver.get('http://185.183.122.133/patient/')
+        self.driver.get(f"{HOST}/patient/")
         self.driver.find_element_by_link_text('Мои файлы').click()
-        self.assertIn('Мои файлы', self.driver.page_source)
+        self.assertIn('Файлы пациента', self.driver.page_source)
 
     def test_chat_callcenter_in_page(self):
-        self.driver.get('http://185.183.122.133/patient/')
+        self.driver.get(f"{HOST}/patient/")
         self.driver.find_element_by_link_text('Чат с колл- центром').click()
-        self.assertIn('Чат с колл- центром', self.driver.page_source)
-
-
-
-
-
-
-
-
+        self.assertIn('Чат с колл-центром', self.driver.page_source)
 
 
 if __name__ == '__main__':
--- tests_abstract.py
+++ tests_abstract.py
@@ -1,12 +1,13 @@
 import unittest
 from abc import ABCMeta
 
-from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-#TODO: Need path TO YOU geckodriver, for example it's my path.
+# TODO: Need path TO YOU geckodriver, for example it's my path.
 PATH_TO_FIREFOX_WEBDRIVER = '/home/ubunu/intership_step_3_selenium/geckodriver'
+HOST = 'http://185.183.122.133'
 
 
 class TestAbstract(unittest.TestCase, metaclass=ABCMeta):
@@ -14,16 +15,16 @@ class TestAbstract(unittest.TestCase, metaclass=ABCMeta):
 
     @classmethod
     def setUpClass(cls) -> None:
-
         cls.driver = webdriver.Firefox(executable_path=PATH_TO_FIREFOX_WEBDRIVER)
 
 
 class TestAbstractWithLogin(unittest.TestCase, metaclass=ABCMeta):
     driver = None
+
     @classmethod
     def setUpClass(cls) -> None:
         cls.driver = webdriver.Firefox(executable_path=PATH_TO_FIREFOX_WEBDRIVER)
-        cls.driver.get("http://185.183.122.133/")
+        cls.driver.get(f"{HOST}")
         login = cls.driver.find_element_by_id('id_username')
         login.clear()
         login.send_keys("patient_for_tests")
@@ -37,4 +38,4 @@ class TestAbstractWithAuthorizationPatient(TestAbstractWithLogin, metaclass=ABCM
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        WebDriverWait(cls.driver, 10).until(EC.url_to_be("http://185.183.122.133/patient/"))
+        WebDriverWait(cls.driver, 10).until(EC.url_to_be(f"{HOST}/patient/"))
